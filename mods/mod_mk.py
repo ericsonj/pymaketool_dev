ff5c22b8a8416b6140fa5d6e29bdb7c4ac009ad9
Ericson <eric.surix@gmail.com>
2021-03-04 16:18:55 -0300

    Add documentation and BasicModule

--- mods/mod_mk.py
+++ mods/mod_mk.py
@@ -1,10 +1,5 @@
 from pymakelib import Module
 
 @Module.ModuleClass
-class mod(Module.AbstractModule):
-
-    def getSrcs(self):
-        return self.findSrcs(Module.SrcType.C)
-    
-    def getIncs(self):
-        return self.findIncs(Module.IncType.C)
+class mod(Module.BasicCModule):
+    pass
